fc677e187ad5df717f1c4cf508c53c2b58a8354e
Pdubey1994 <73110721+DataEmission@users.noreply.github.com>
2024-08-03 18:47:43 +0530

    Initial commit

---
+++ src/main/java/com/urban/committee/UcEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcEurekaServerApplication.class, args);
+    }
+
+}
