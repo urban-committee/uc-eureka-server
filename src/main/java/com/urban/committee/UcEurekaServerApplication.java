50222d56bab586b366cdd291206eca2afa24fbbf
Pdubey1994 <committeeurban@gmail.com>
2024-08-04 15:57:44 +0530

    Eureka Registry
    Eureka Registry

--- src/main/java/com/urban/committee/UcEurekaServerApplication.java
+++ src/main/java/com/urban/committee/UcEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.urban.committee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UcEurekaServerApplication {
 
     public static void main(String[] args) {
